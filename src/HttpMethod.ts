6a89039fc41ec9bb11469496ffdee12443f6765a
Rob Muchall <rmuchall@protonmail.com>
2020-08-25 11:22:56 +0100

    Added HttpMethod enum

---
+++ src/HttpMethod.ts
@@ -0,0 +1,10 @@
+export enum HttpMethod {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    CONNECT = "CONNECT",
+    OPTIONS = "OPTIONS",
+    TRACE = "TRACE",
+    PATCH = "PATCH"
+}
